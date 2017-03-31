4caa3e49b5732055af2869fd523b49f2c6349eb6
Joshua Varghese <joshuavarghese527@gmail.com>
2017-03-31 18:34:52 +0000

    Room View Form

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,7 @@
 class RoomsController < ApplicationController
   before_action :set_room, only: [:show, :edit ,:updae]
   before_action :authenticate_user!, execpt: [:show]
+
   def index
     @rooms = current_user.rooms
   end
@@ -24,7 +25,6 @@ class RoomsController < ApplicationController
   end
 
   def edit 
-   
   end
 
   def update
@@ -36,10 +36,12 @@ class RoomsController < ApplicationController
   end
 
   private
+
   def set_room
      @room = Room.find(params[:id])
   end
 
   def room_params
     params.require(:room).permit(:home_type,:room_type,:accommodate,:bed_room,:bath_room,:listing_name,:summary,:address, :is_tv, :is_kitchen, :is_air, :is_heating, :is_internet, :price, :active)
+  end
 end
