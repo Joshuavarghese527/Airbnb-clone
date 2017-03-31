be9020362279ec831e16696e349320543401f9d7
Joshua Varghese <joshuavarghese527@gmail.com>
2017-03-31 17:27:02 +0000

    Google sign in

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -273,5 +273,6 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
   config.omniauth :facebook,  ENV["facebook_app_id"], ENV["facebook_app_secert"], scope: 'email', info_fields: 'email,name'
+  config.omniauth :google_oauth2,  ENV["google_app_id"], ENV["google_app_secert"], scope: 'email', info_fields: 'email,name'
 end
 
